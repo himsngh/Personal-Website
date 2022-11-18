35141d6bd0dc82359a08baaf33a8d39cf5e0cb1b
Himansh Singh <himansh.singh3@gmail.com>
2022-11-18 10:44:28 +0530

    wip: intial-commit main content

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import exp from "constants"
+
+function Header() {
+    return (
+        <div>
+            <h2 className="title">Hi</h2>
+            <h1>I am Himanshu Ranjan</h1>
+            <p>A full-stack (more backend experience) developer</p>
+        </div>
+    )
+}
+
+export default Header;
